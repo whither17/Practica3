05d27e98cf2b278250ca49ff8a7ecfc57507cab5
Robinson Correa Morales <robinson.corream@udea.edu.co>
2023-09-28 17:31:51 -0500

    creacion metodo 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include "funciones.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    string ruta =  "H:\\ProyectosInfo2\\practica3\\build-practica3-Desktop_Qt_6_5_2_MinGW_64_bit-Debug\\prueba.txt";
+    int semilla = 4;
+
+
+    Acaracter(ruta,cambiarBits(ruta, Abinario(ruta,archivo(ruta)), semilla) ,semilla);
+
     return 0;
 }
+
+
